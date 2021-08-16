fee72db120a8e48802e3d837efb5f720ca26249d
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-08-16 17:38:46 +0100

    added pagination

--- APP/views/index.py
+++ APP/views/index.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import login
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from APP.models import Expense
 
@@ -8,6 +9,10 @@ from APP.models import Expense
 @login_required(login_url='auth/signin')
 def index(request):
     expenses = Expense.objects.filter(owner=request.user)
-
-    context = {"expenses": expenses}
+    paginator = Paginator(expenses, 5)
+    page_number = request.GET.get('page')
+    page_obj = Paginator.get_page(paginator, page_number)
+    context = { 
+        "expenses": expenses, 
+        "page_obj": page_obj}
     return render(request, 'index.html', context)
